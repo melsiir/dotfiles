4f5c3113931133d551ad2da3de6dc4e749f6abbd
Mohammed Elsir <likmik271@gmail.com>
2022-01-10 20:24:41 +0200

    added exe alias

--- fish/functions/my-nvim.fish
+++
@@ -1,3 +0,0 @@
-function my-nvim --wraps='cd /data/data/com.termux/files/home/.config/my-nvim' --wraps='cd /data/data/com.termux/files/home/.config/dotfiles' --description 'alias my-nvim=cd /data/data/com.termux/files/home/.config/dotfiles'
-  cd /data/data/com.termux/files/home/.config/dotfiles $argv; 
-end
