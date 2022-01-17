1d0abeb7058f2c43d260da88098c58ec98fd5d92
Mohammed Elsir <likmik271@gmail.com>
2022-01-17 18:53:56 +0200

    some changes

--- fish/functions/v.fish
+++
@@ -1,3 +0,0 @@
-function v --wraps=/data/data/com.termux/files/home/.local/bin/lvim --description 'alias v=/data/data/com.termux/files/home/.local/bin/lvim'
-  /data/data/com.termux/files/home/.local/bin/lvim $argv; 
-end
