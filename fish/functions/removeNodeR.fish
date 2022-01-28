e58f401a7345bcb73373c30fd088891dbf67d3ea
Mohammed Elsir <likmik271@gmail.com>
2022-01-28 14:19:28 +0200

    added aliases

---
+++ fish/functions/removeNodeR.fish
@@ -0,0 +1,3 @@
+function removeNodeR
+find . -type d -name 'node_modules' -print0 | xargs -0 -I {} rm -rf "{}"
+end
--- fish/functions/rnm.fish
+++
@@ -1,3 +0,0 @@
-function rnm --wraps='rm -r node_modules' --description 'alias rnm=rm -r node_modules'
-  rm -r node_modules $argv; 
-end
